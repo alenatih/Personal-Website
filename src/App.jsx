e108b5082435f61f7f750c0b2806fe6cdac93f7b
Alena <alyonatihomirova9@gmail.com>
2024-06-26 01:17:01 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React, { useState } from "react"
 // import reactLogo from "./assets/react.svg"
 // import viteLogo from "/vite.svg"
 import Header from "./components/Header.jsx"
-import MainSection from "./components/MainSection.jsx"
+import AboutMe from "./components/AboutMe.jsx"
+import Portfolio from "./components/Portfolio.jsx"
+import Skills from "./components/Skills.jsx"
 import Footer from "./components/Footer.jsx"
 import "./App.css"
 
@@ -11,7 +13,9 @@ function App() {
   return (
     <div className="app-container">
       <Header />
-      <MainSection />
+      <AboutMe />
+      <Portfolio />
+      <Skills />
       <Footer />
     </div>
   )
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function AboutMe() {
+    return (
+        <main className="about-me">
+            About me
+        </main>
+    )
+}
+
+export default AboutMe
