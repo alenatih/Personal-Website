046695af38dc471e32a27bccdb507f94ef7f4b8d
Alena <alyonatihomirova9@gmail.com>
2024-06-27 19:01:06 +0300

    Update

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "/index.html",
+    "./src/main.tsx",
+    "./src/app.txs",
+    "./src/components/Header.tsx",
+    "./src/components/AboutMe.tsx",
+    "./src/components/Portfolio.tsx",
+    "./src/components/Skills.tsx",
+    "./src/components/Footer.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
