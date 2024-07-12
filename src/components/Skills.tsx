38e436e5a1e0241ea9a086257715877f1fb7e7fe
Alena Tikhomirova <137419549+AlenaTih@users.noreply.github.com>
2024-07-13 02:09:44 +0300

    Update

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,16 +1,6 @@
-// import React from "react"
-// import { useRef } from "react"
-// import { ROUTE } from "./constants.ts"
-
 function Skills() {
-    // const sectionRef = useRef<HTMLDivElement>(null)
-    // : RefObject<HTMLDivElement>
-
     return (
-        <div className="skills"
-            // ref={sectionRef}
-            // prevTo={ROUTE.portfolio}
-            >
+        <div className="skills">
             Skills
         </div>
     )
