1b4b20e299f223b4e07b86519f238305341c11b0
Alena Tikhomirova <137419549+AlenaTih@users.noreply.github.com>
2024-07-16 02:55:47 +0300

    Update

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react"
+import { Navigate } from "react-router-dom"
+
 function AboutMe() {
+    const [user, setUser] = useState("userName")
+
+    function handleClick() {
+        setUser("")
+    }
+
+    if (!user) {
+        return <Navigate to="/" replace={true} />
+    }
+
     return (
         <div className="about-me">
             About me
+
+            <button onClick={handleClick}>Sign out</button>
         </div>
     )
 }
