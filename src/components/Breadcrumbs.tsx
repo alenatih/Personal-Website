5611e216bcad9808d6fd1a8df01f35a43613c488
Alena <alyonatihomirova9@gmail.com>
2024-07-18 13:31:08 +0300

    Update

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -3,7 +3,7 @@ import { useLocation, NavLink } from "react-router-dom"
 function Breadcrumbs() {
     const location = useLocation()
 
-    let currentLink = ""
+    let currentLink:string = ""
 
     const crumbs = location.pathname.split("/")
         .filter(crumb => crumb !== "")
