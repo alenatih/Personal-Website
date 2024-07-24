b7b9cfeff6ba2fd7218ceff51fcc715dc202f289
Alena Tikhomirova <137419549+AlenaTih@users.noreply.github.com>
2024-07-25 01:56:42 +0300

    Update the skills page to add text

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,7 +1,11 @@
 function Skills() {
     return (
         <div className="skills">
-            Skills
+            <h2 className="skills-title">Skills</h2>
+            <p className="skills-text">
+                My main skills include HTML, CSS, JavaScript, RESTful APIs, Git, GitHub, React Router, Firebase, and Figma. 
+                Learning React, TypeScript, and Tailwind CSS.
+            </p>
         </div>
     )
 }
