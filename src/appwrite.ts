fbe7832e47af8cdb4875e6843ebdb21f795ebb7e
Alena Tikhomirova <137419549+AlenaTih@users.noreply.github.com>
2024-07-26 02:40:37 +0300

    Adding Appwrite

---
+++ src/appwrite.ts
@@ -0,0 +1,16 @@
+import { Client, Databases, Account } from "appwrite"
+// Import type models for Appwrite
+import { type Models } from 'appwrite'
+
+const client: Client = new Client()
+
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('66a2d9bb002ea27689fd') // Replace with your project ID
+
+export const account: Account = new Account(client)
+export const database: Databases = new Databases(client)
+
+// You then use the imported type definitions like this
+// const authUser: Models.Session = await account.createEmailPasswordSession(email, password);
+
