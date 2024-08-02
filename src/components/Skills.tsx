6ed0ee4aed2a2e6fee3b839a5aa3360dbadbda44
Alena Tikhomirova <137419549+AlenaTih@users.noreply.github.com>
2024-08-03 01:33:26 +0300

    Update skills page

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,7 +1,7 @@
 function Skills() {
     return (
         <div className="skills">
-            <h2 className="skills-title">Skills</h2>
+            <h2 className="skills-title">My skills</h2>
             <p className="skills-text">
                 My main skills include HTML, CSS, JavaScript, RESTful APIs, Git, GitHub, React Router, Firebase, and Figma. 
                 Learning React, TypeScript, and Tailwind CSS.
