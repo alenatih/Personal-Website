600c1fe970ffb84179dc250a0cba9762f53c59cf
Alena Tikhomirova <137419549+AlenaTih@users.noreply.github.com>
2024-08-06 01:39:23 +0300

    Update Blog page

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -2,6 +2,10 @@ function Blog() {
     return (
         <div className="blog">
             <h2 className="blog-title">Blog</h2>
+
+            <h4 className="blog-quote-title">My favourite quote</h4>
+            <p className="blog-quote-text">If you want momentum, you’ll have to create it yourself, right now, by getting up and getting started.</p>
+            <p className="blog-quote-author">- Ryan Holiday</p>
         </div>
     )
 }
