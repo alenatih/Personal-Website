df9a481dd94bfed6b5f015bef71a2ce9f6e69b47
Alena <alyonatihomirova9@gmail.com>
2024-08-06 19:16:08 +0300

    Update Blog page

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,10 +1,17 @@
 function Blog() {
     return (
         <div className="blog">
-            <h2 className="blog-title">Blog</h2>
+            <h2 className="blog-title">Blog
+                <span className="blog-title-span"> will be here</span>
+            </h2>
 
-            <h4 className="blog-quote-title">My favourite quote</h4>
-            <p className="blog-quote-text">If you want momentum, you’ll have to create it yourself, right now, by getting up and getting started.</p>
+            <h4 className="blog-quote-title">
+                <span className="blog-quote-title-span">But until then, here's </span>
+                My favourite quote
+            </h4>
+            <p className="blog-quote-text">"If you want momentum, you’ll have to create it yourself, 
+                right now, by getting up and getting started."
+            </p>
             <p className="blog-quote-author">- Ryan Holiday</p>
         </div>
     )
