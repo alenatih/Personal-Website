c36f8616def710afe75e8659d2992516fba3661e
Alena Tikhomirova <137419549+AlenaTih@users.noreply.github.com>
2024-08-09 02:55:48 +0300

    Update Home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,14 @@
+import { NavLink } from "react-router-dom"
+
 function Home() {
 
     return (
         <div className="home">
             <h2 className="home-title">Alena Tikhomirova, a Frontend developer</h2>
+
+            <div className="home-links">
+                <NavLink className="view-work-link" to="/portfolio">View my work</NavLink>
+            </div>
         </div>
     )
 }
