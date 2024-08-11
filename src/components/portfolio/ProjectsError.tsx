89ac37dbfe6d74cf1f0a13f4da42f752d64fd9eb
Alena Tikhomirova <137419549+AlenaTih@users.noreply.github.com>
2024-08-12 02:24:58 +0300

    Update ProjectsError and NotFound pages

--- src/components/portfolio/ProjectsError.tsx
+++ src/components/portfolio/ProjectsError.tsx
@@ -6,7 +6,7 @@ function ProjectsError() {
 
     return (
         <div  className="projects-error">
-            <h2>Error</h2>
+            <h4 className="projects-error-title">Error</h4>
             <p>{error.message}</p>
             <NavLink to="/">Come back to the Homepage</NavLink>
         </div>
