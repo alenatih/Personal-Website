8404a88e598ac9546e48de5498effe65c28293b8
Alena <alyonatihomirova9@gmail.com>
2024-08-12 14:15:21 +0300

    Update Resources page

--- src/components/Resources.tsx
+++ src/components/Resources.tsx
@@ -2,6 +2,10 @@ function Resources() {
     return (
         <div className="resources">
             <h4 className="resources-title">Resources</h4>
+            <a className="resources-link" href="#">Scrimba</a>
+            <a className="resources-link" href="#">React Router course by Code Ninja</a>
+            <a className="resources-link" href="#">Appwrite</a>
+            <a className="resources-link" href="#">Open source GitHub repository</a>
         </div>
     )
 }
