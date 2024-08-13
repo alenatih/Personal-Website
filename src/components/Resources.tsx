d50208ca657aa29b36b27fbcf4c80b8ea187d7d7
Alena <alyonatihomirova9@gmail.com>
2024-08-13 18:19:13 +0300

    Update Resources page

--- src/components/Resources.tsx
+++ src/components/Resources.tsx
@@ -1,11 +1,52 @@
 function Resources() {
     return (
         <div className="resources">
-            <h2 className="resources-title">Resources</h2>
-            <a className="resources-link" href="#">Scrimba</a>
-            <a className="resources-link" href="#">React Router course by Code Ninja</a>
-            <a className="resources-link" href="#">Appwrite</a>
-            <a className="resources-link" href="#">Open source GitHub repository</a>
+            <h2 className="resources-title">Resources I recommend</h2>
+
+            <div className="resource">
+                <a className="resources-link" href="https://v2.scrimba.com/">Scrimba</a>
+                <p className="resource-text">
+                    — an awesome platform to learn 
+                    and practice Frontend development.
+                </p>
+            </div>
+
+            <div className="resource">
+                <a className="resources-link" href="https://www.youtube.com/playlist?list=PL4cUxeGkcC9iVKmtNuCeIswnQ97in2GGf">
+                    React Router course by Net Ninja
+                </a>
+                <p className="resource-text">
+                    — an awesome course to learn React Router.
+                </p>
+            </div>
+
+            <div className="resource">
+                <a className="resources-link" href="https://appwrite.io/">Appwrite</a>
+                <p className="resource-text">
+                    — an awesome open-source platfrom to build apps with a lot of features
+                    including Authentication, Databases, Functions, Storage, and Messaging.
+                </p>
+                <p className="resource-text">
+                    I use Appwrite in this project to store information about my projects.
+                </p>
+            </div>
+
+            <div className="resource">
+                <a className="resources-link" href="https://healthcheck.eddiehubcommunity.org/">
+                    HealthCheck
+                </a>
+                <p className="resource-text">
+                    — an awesome open-source project to use, learn from, and contribute to.
+                </p>
+
+                <a href="https://github.com/EddieHubCommunity/HealthCheck">GitHub</a>
+
+                <p className="resource-text community-text">
+                    Also a great community about open source:
+                </p>
+                <a href="https://github.com/EddieHubCommunity">EddieHub</a>
+            </div>
+
         </div>
     )
 }
