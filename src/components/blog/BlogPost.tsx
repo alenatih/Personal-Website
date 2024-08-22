e36596d9e0778d0b57becb77fb8898154a9bfec3
Alena Tikhomirova <137419549+AlenaTih@users.noreply.github.com>
2024-08-23 02:04:36 +0300

    Use safe type in BlogPost file

--- src/components/blog/BlogPost.tsx
+++ src/components/blog/BlogPost.tsx
@@ -1,9 +1,23 @@
 import { useLoaderData, useParams } from "react-router-dom"
 import { database } from "../../appwriteConfig.ts"
 
+interface BlogPost {
+    $id: string;
+    $collectionId: string;
+    $createdAt: string;
+    $databaseId: string;
+    $permissions: string[];
+    $tenant: string;
+    $updatedAt: string;
+    Title: string;
+    Text: string;
+    [key: string]: any; // Index signature to allow for additional properties
+}
+
 function BlogPost() {
     const { slugId } = useParams()
-    const blogPostInfo:any = useLoaderData()
+    // const blogPostInfo:any = useLoaderData()
+    const blogPostInfo = useLoaderData() as BlogPost
 
     const blogPostId = slugId?.split("-").pop()
 
@@ -28,7 +42,7 @@ export const blogPostInfoLoader = async ({params}:any) => {
     try {
         const response = await database.getDocument("66a2de2e00117b4ed64f", "66c135400034ed3eff4a", blogPostId)
         // ("databaseId", "collectionId")
-        return response
+        return response as BlogPost
     } catch (error) {
         throw Error("Could not find this blog post.")
     }
