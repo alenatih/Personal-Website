2e0e4128ffc49ac339c9abd2598d6135b6948686
Alena Tikhomirova <137419549+AlenaTih@users.noreply.github.com>
2024-08-25 01:31:03 +0300

    Update ProjectError and BlogError to make them type-safe

--- src/components/portfolio/ProjectsError.tsx
+++ src/components/portfolio/ProjectsError.tsx
@@ -1,14 +1,21 @@
 import { NavLink, useRouteError } from "react-router-dom"
 import ErrorDuck from "../../assets/error-duck.png"
 
+// A type that covers different possible error cases
+type RouteError = {
+    message?: string; // Handles standard error messages
+    statusText?: string; // For errors from responses with a statusText (from failed HTTP responses)
+    data?: any; // For custom errors that include data
+  } | null // Handles the case where there's no error
+
 function ProjectsError() {
     
-    const error:any = useRouteError()
+    const error = useRouteError() as RouteError
 
     return (
         <div  className="projects-error">
             <h4 className="projects-error-title">Error</h4>
-            <p>{error.message}</p>
+            <p>{error?.message || error?.statusText || error?.data}</p>
             <NavLink to="/">Come back to the Homepage</NavLink>
 
             <img
