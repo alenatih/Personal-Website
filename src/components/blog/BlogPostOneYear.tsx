b6393d7f65cbe1b17ba7eaed86fec539142fabc4
Alena Tikhomirova <137419549+AlenaTih@users.noreply.github.com>
2024-09-12 04:25:36 +0300

    Update blog post

--- src/components/blog/BlogPostOneYear.tsx
+++ src/components/blog/BlogPostOneYear.tsx
@@ -4,7 +4,7 @@ function BlogPostOneYear() {
     return (
         <div className="blog-post-one-year">
             <h4 className="blog-post-page-one-year-title">
-                Lessons I learned in a year of learning to program.
+                Lessons I learned in a year of learning to program
             </h4>
             <img 
                 className="blog-post-image"
