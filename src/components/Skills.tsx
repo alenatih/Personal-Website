30ac03437faf3815d5f1446625ff9596fb4172f7
Alena <alyonatihomirova9@gmail.com>
2024-10-23 17:28:50 +0300

    Update Skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,11 +3,12 @@ function Skills() {
         <div className="skills">
             <h2 className="skills-title">My skills</h2>
             <p className="skills-text">
-                My main skills include HTML, CSS, JavaScript, RESTful APIs, Git, GitHub, 
-                React Router, Vite, Vercel, Firebase, and Figma.
+                My main skills include HTML, CSS, JavaScript, TypeScript, 
+                React, RESTful APIs, Git, GitHub, React Router, Vite, Vercel, Appwrite, 
+                Firebase, Tailwind CSS, and Figma.
             </p>
             <p className="skills-text">
-                Learning React, TypeScript, and Tailwind CSS.
+                Learning Next.js.
             </p>
         </div>
     )
